fbf132953bdcef44a40acd11f108524d769774a8
ninanorgren <norgrennina85@gmail.com>
2020-10-15 13:11:27 +0200

    made pdfs for day1

--- scripts/publish_lecture.sh
+++ scripts/publish_lecture.sh
@@ -5,7 +5,7 @@
 # files to the gh-pages branch.
 # Usage: ./scripts/publish_lecture lectures/Day_1.slides.html
 # Remember to push the gh-pages branch when you're done.
-# Also, check that ht19/topics.md is updated to show the lecture you want to publish.
+# Also, check that ht20/topics.md is updated to show the lecture you want to publish.
 set -e
 TMP="tmp_lecture.tmp"
 TMP_IMG="tmpimgtmp"
@@ -18,11 +18,11 @@ cp $1 $TMP
 cp lectures/img/*{png,jpg,PNG} $TMP_IMG
 cp lectures/*css $TMP_CSS
 git checkout gh-pages
-mv $TMP ht19/lecture/$DEST
-mv $TMP_IMG/* ht19/lecture/img/
-mv $TMP_CSS/* ht19/lecture/
-git add ht19/lecture/$DEST
-git add ht19/lecture/img/*{png,jpg,PNG}
-git add ht19/lecture/*css
+mv $TMP ht20/lecture/$DEST
+mv $TMP_IMG/* ht20/lecture/img/
+mv $TMP_CSS/* ht20/lecture/
+git add ht20/lecture/$DEST
+git add ht20/lecture/img/*{png,jpg,PNG}
+git add ht20/lecture/*css
 git commit -m "Update lecture $DEST"
-git checkout ht19
+git checkout ht20
