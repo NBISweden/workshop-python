dc92122c653253e120f9f5f1995d1d14ee065ba4
Nanjiang Shu <nanjiang.shu@gmail.com>
2024-11-10 22:29:18 +0100

    add script print_argv.py to downloads

---
+++ downloads/print_argv.py
@@ -0,0 +1,11 @@
+#/usr/bin/env python
+import sys
+
+print("size of sys.argv = ", len(sys.argv))
+print("program_name = ", sys.argv[0])
+
+counter = 1
+while counter < len(sys.argv):
+    print("arg" + str(counter) + " = ", sys.argv[counter])
+    counter += 1
+
